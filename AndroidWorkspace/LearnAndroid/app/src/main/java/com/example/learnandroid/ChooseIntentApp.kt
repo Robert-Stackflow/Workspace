0b3fa57844f16d5aded058643055ba4282b9d774
Robert <72618337+Robert-Stackflow@users.noreply.github.com>
2022-07-08 16:17:27 +0800

    Add Android Project——LearnAndroid

---
+++ AndroidWorkspace/LearnAndroid/app/src/main/java/com/example/learnandroid/ChooseIntentApp.kt
@@ -0,0 +1,11 @@
+package com.example.learnandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseIntentApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choose_intent_app_layout)
+    }
+}
