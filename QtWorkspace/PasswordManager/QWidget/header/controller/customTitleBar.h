c1ce496dd3c182712fe128d90c396520d22d8232
Robert <72618337+Robert-Stackflow@users.noreply.github.com>
2022-07-30 14:29:50 +0800

    no message

--- QtWorkspace/PasswordManager/QWidget/header/controller/customTitleBar.h
+++ QtWorkspace/PasswordManager/QWidget/header/controller/customTitleBar.h
@@ -1,5 +1,5 @@
-﻿#ifndef TITLEBAR_H
-#define TITLEBAR_H
+﻿#ifndef CUSTOMTITLEBAR_H
+#define CUSTOMTITLEBAR_H
 #include <QLabel>
 #include <QEvent>
 #include <QPoint>
@@ -11,15 +11,16 @@
 #include <QMouseEvent>
 #include <QApplication>
 
-class TitleBar : public QWidget
+class CustomTitleBar : public QWidget
 {
     Q_OBJECT
 public:
-    explicit TitleBar(QWidget *parent = NULL);
+    explicit CustomTitleBar(QWidget *parent = NULL);
     void setWindowTitle(const QString& title);
-    void setTitleBarIcon(const QString& iconPath);
-    void forbiddenMaxmizeButton();
-    void forbiddenUserButton();
+    void setWindowIcon(const QString &iconPath);
+    void addCustomButton(QPushButton* newButton);
+    void setMaximizeVisible(bool visible);
+    void setMinimizeVisible(bool visible);
 protected:
     virtual void mouseDoubleClickEvent(QMouseEvent *event);
     virtual void mousePressEvent(QMouseEvent *event);
@@ -32,17 +33,21 @@ protected slots:
     void onClicked();
 
 public:
-    bool m_leftButtonPressed;
-    bool m_maximizeForbidden=false;
-    QPoint m_end;
-    QPoint m_start;
-    QLabel* m_iconLabel;
-    QLabel* m_titleLabel;
-    QPushButton* m_optionButton;
-    QPushButton* m_userButton;
-    QPushButton* m_minimizeButton;
-    QPushButton* m_maximizeButton;
-    QPushButton* m_closeButton;
+    bool leftButtonPressed;
+    bool maximizeVisible=true;
+    bool minimizeVisible=true;
+    QPoint end;
+    QPoint start;
+    QLabel* iconLabel;
+    QLabel* titleLabel;
+    QPushButton* minimizeButton;
+    QPushButton* maximizeButton;
+    QPushButton* closeButton;
+    QVBoxLayout* mainLayout;
+    QHBoxLayout* topLayout;
+    QHBoxLayout* leftLayout;
+    QHBoxLayout* customLayout;
+    QHBoxLayout* rightLayout;
 };
 
-#endif // TITLEBAR_H
+#endif // CUSTOMTITLEBAR_H
